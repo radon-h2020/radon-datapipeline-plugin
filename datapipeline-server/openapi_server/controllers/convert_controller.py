9a6a361011644c14861589a9ba9709d1ef310211
Pelle Jakovits <pelle.jakovits@gmail.com>
2020-05-19 18:47:59 +0300

    Creating the API implementation

--- datapipeline-server/openapi_server/controllers/convert_controller.py
+++ datapipeline-server/openapi_server/controllers/convert_controller.py
@@ -17,4 +17,29 @@ def submit_datapipeline(data_pipeline=None):  # noqa: E501
     """
     if connexion.request.is_json:
         data_pipeline = DataPipeline.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+
+
+    file =  connexion.request.files['file']
+    filepath = '/tmp/' + file.filename
+    file.save(filepath)
+
+    mimetypes = {
+        ".csar": "application/octet-stream",
+        ".yml":  "text/yaml",
+        ".yaml":  "text/yaml",
+    }
+
+    ext = os.path.splitext(filepath)[1]
+
+
+    mimetype = mimetypes.get(ext, "text/yaml")
+
+
+    service = DataPipeline(file = filepath)
+    service.convert(filepath)
+
+    with open(filepath, 'rb') as f:
+      content =  f.read()
+
+    return Response(content, mimetype=mimetype, headers={"Content disposition":"attachment; filename=" + file.filename})
+
